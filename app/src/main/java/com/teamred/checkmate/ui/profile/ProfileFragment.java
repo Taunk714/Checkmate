7485cd41e833a3aa2faa895bdc21483cdf71ed14
Lightningbread76 <67837759+Lightningbread76@users.noreply.github.com>
2021-11-30 12:06:12 -0500

    added content in profile xml

--- app/src/main/java/com/teamred/checkmate/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/teamred/checkmate/ui/profile/ProfileFragment.java
@@ -4,23 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
+import com.teamred.checkmate.R;
 import com.teamred.checkmate.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
 private FragmentProfileBinding binding;
+private LinearLayout relative;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
 
-        binding =FragmentProfileBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        relative = binding.backgroundLayout;
+        //relative.setBackgroundResource(R.drawable.background);
+
+
+
 
 
         return root;
