30897c39e8017d2202bb07b7f2764a8e8e4dbf07
Lightningbread76 <67837759+Lightningbread76@users.noreply.github.com>
2021-11-30 11:19:58 -0500

    fragment dashboard.xml = profile layout file

--- app/src/main/java/com/teamred/checkmate/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/teamred/checkmate/ui/dashboard/DashboardFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.teamred.checkmate.R;
-import com.teamred.checkmate.databinding.FragmentDashboardBinding;
+import com.teamred.checkmate.data.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
