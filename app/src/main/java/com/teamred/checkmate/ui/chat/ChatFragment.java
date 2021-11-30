fc50fc3dd48b19c68b5976ef6b7905b1e56fb186
Rachel <taunk.714@gmail.com>
2021-11-30 12:07:04 -0500

    half chat

--- app/src/main/java/com/teamred/checkmate/ui/chat/ChatFragment.java
+++ app/src/main/java/com/teamred/checkmate/ui/chat/ChatFragment.java
@@ -1,13 +1,16 @@
 package com.teamred.checkmate.ui.chat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -33,32 +36,19 @@ public class ChatFragment extends Fragment {
         // auth to check whether user is signed in
         //calenderHeatmap = (Button) getView().findViewById(R.id.calendarheatmap);
         binding.messageEditText.addTextChangedListener(new MyButtonObserver(binding.sendButton));
-//
-//        registerForActivityResult(new MyOpen)
+
+        registerForActivityResult(new MyOpenDocumentContract(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+//                onImageSelected(result);
+            }
+        });
 //        binding.addMessageImageView.setOnClickListener(
 //
 //        );
 
-//
-//        FloatingActionButton btnAddPost = binding.btnAddPost;
-//        btnAddPost.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                Intent i = new Intent(getActivity(), AddPostActivity.class);
-//
-//                if (FirebaseAuth.getInstance().getCurrentUser() == null){
-//                    Intent login = new Intent(getActivity(), LoginActivity.class);
-//                    Toast.makeText(getContext(), "Interceptor! Jump to login", Toast.LENGTH_LONG).show();
-//                    login.putExtra("callback", CreateGroupActivity.class.getCanonicalName());
-//                    startActivity(login);
-//                }else{
-//                    Intent i = new Intent(getActivity(), CreateGroupActivity.class);
-//                    Toast.makeText(getContext(), "click create group", Toast.LENGTH_LONG).show();
-//                    startActivity(i);
-//                }
-//            }
-//        });
-//
+
+
 
         return root;
     }
@@ -68,4 +58,29 @@ public class ChatFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        val inflater = menuInflater
+//        inflater.inflate(R.menu.main_menu, menu)
+//        return true
+//    }
+//
+//    private Boolean onOptionsItemSelected(MenuItem item) {
+//        return when (item.itemId) {
+//            R.id.sign_out_menu -> {
+//                signOut()
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//
+//    private void onImageSelected(Uri uri) {
+//        // TODO: implement
+//    }
+//
+//    private void putImageInStorage(storageReference: StorageReference, uri: Uri, key: String?) {
+//        // Upload the image to Cloud Storage
+//        // TODO: implement
+//    }
 }
