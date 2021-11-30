11a844c73309855ac7838040240f7bde029ea450
Rachel <taunk.714@gmail.com>
2021-11-30 11:12:46 -0500

    temp

---
+++ app/src/main/java/com/teamred/checkmate/data/model/FriendlyMessage.java
@@ -0,0 +1,49 @@
+package com.teamred.checkmate.data.model;
+
+public class FriendlyMessage {
+    private String text;
+    private String name;
+    private String photoUrl;
+    private String imageUrl;
+
+    public FriendlyMessage() { }
+
+    public FriendlyMessage(String text, String name, String photoUrl, String imageUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
