e4155874eb63aa913be26ab98e88f1eb599d244a
xin-cao <xincao.xc@gmail.com>
2021-12-06 19:18:28 -0500

    Updated bad group references and also added real author to post

--- app/src/main/java/com/teamred/checkmate/ui/notes/PostListViewModel.java
+++ app/src/main/java/com/teamred/checkmate/ui/notes/PostListViewModel.java
@@ -16,6 +16,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.teamred.checkmate.data.CheckmateKey;
 import com.teamred.checkmate.data.model.Post;
 import com.teamred.checkmate.ui.PostListViewAdapter;
 
@@ -34,7 +35,7 @@ public class PostListViewModel extends ViewModel {
         this.groupID = groupID;
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        this.docRef = db.collection("Groups").document(this.groupID);
+        this.docRef = db.collection(CheckmateKey.GROUP_FIREBASE).document(this.groupID);
         this.docRef.collection("posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -45,7 +46,7 @@ public class PostListViewModel extends ViewModel {
 
                     String title = data.get("postTitle").toString();
                     List<String> tags = (ArrayList<String>) data.get("tags");
-                    String author = "TODO Author"; // TODO
+                    String author = data.get("author").toString();
                     String content = data.get("content").toString();
                     String subtopic = data.get("subTopic").toString();
                     String onenoteWebURL = data.get("onenoteWebURL") != null ? data.get("onenoteWebURL").toString() : null;
@@ -78,7 +79,7 @@ public class PostListViewModel extends ViewModel {
 
     private void loadPosts() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("Groups").document(this.groupID);
+        DocumentReference docRef = db.collection(CheckmateKey.GROUP_FIREBASE).document(this.groupID);
         // Do an async operation to fetch posts.
         docRef.collection("posts").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
